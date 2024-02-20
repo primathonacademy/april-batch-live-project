5328e6d383480324662f0c6ca92e3cf8f39ccd5e
Sahil Verma <sahilverma15012002@gmail.com>
2024-02-20 15:56:50 +0530

    [Feature] : Header Component Comments Resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                'header-gray': '#2F3750',
+                'fade-gray': '#828796',
+                'brand-blue': '#0A66EC',
+                'brand-pink': '#F46997',
+                'accent-blue': '#253F9C'
+            },
+            borderRadius: {
+                '4xl': '28px',
+                '5xl': '32px'
+            }
+        }
     },
     plugins: []
 };
