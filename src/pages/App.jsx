40c4225f7742c63a8f94bd004d62ffc6d2b803ad
dhananhub <dkjha444@gmail.com>
2024-02-20 11:13:39 +0530

    [feature]: Header Component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,7 @@
+import Header from '../component/Header';
+
 const App = () => {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return <Header />;
 };
 
 export default App;
