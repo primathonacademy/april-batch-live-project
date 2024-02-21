b8c53172cdc482e9f1e3da89bc8276f75d43c193
Ramvishvas Kumar <59244208+ram-primathon@users.noreply.github.com>
2024-02-21 14:10:28 +0530

    [Feature] : Add pages and footer component skelton

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,5 @@
+const About = () => {
+    return <h1>Welcom to About</h1>
+}
+
+export default About;
---
+++ src/pages/insight/index.jsx
@@ -0,0 +1,5 @@
+const Insight = () => {
+    return <h1>Welcom to Insight</h1>
+}
+
+export default Insight;
