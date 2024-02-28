ba517ac250db2661f9ebc3eb4aa34505db20a84c
Sahil Verma <sahilverma15012002@gmail.com>
2024-02-28 15:08:15 +0530

    [Feature] : Get in touch component

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,5 +1,11 @@
+import GetInTouch from './GetInTouch';
+
 const Homepage = () => {
-    return <div className="px-6 md:px-16 lg:px-20 xl:px-28 2xl:px-36">Welcome to Homepage</div>;
+    return (
+        <div>
+            <GetInTouch />
+        </div>
+    );
 };
 
 export default Homepage;
