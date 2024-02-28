378440c38f731a08eab645e7d367cabb788bb070
Himanshu <hs3928782@gmail.com>
2024-02-28 14:54:50 +0530

    [Feature] : Ourservice component

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,12 @@ export default {
                 'header-gray': '#2F3750',
                 'fade-gray': '#828796',
                 'brand-blue': '#0A66EC',
+                'dark-blue':'#253F9C',
                 'brand-pink': '#F46997',
-                'accent-blue': '#253F9C'
+                'accent-blue': '#253F9C',
+                'fade-orange':'#E5C090',
+                'fade-white': "#F7F7F7",
+                'fade-blue-white':'#F4F8FF'
             },
             borderRadius: {
                 '4xl': '1.75rem',
