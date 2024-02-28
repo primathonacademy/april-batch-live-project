f9a1cc403ff4e1f16ee6edd0a27c787591816a0c
Sahil Verma <131689244+SahilVermaFED@users.noreply.github.com>
2024-02-28 16:30:05 +0530

    [Feature] : Work component comments resolved

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-export { default as workImg1 } from '../../src/assets/homepage/our-work-1.png';
-export { default as workImg2 } from '../../src/assets/homepage/our-work-2.png';
-export { default as workImg3 } from '../../src/assets/homepage/our-work-3.png';
-export { default as workImg4 } from '../../src/assets/homepage/our-work-4.png';
+export { default as workImg1 } from './homepage/our-work-1.png';
+export { default as workImg2 } from './homepage/our-work-2.png';
+export { default as workImg3 } from './homepage/our-work-3.png';
+export { default as workImg4 } from './homepage/our-work-4.png';
