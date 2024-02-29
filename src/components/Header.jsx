e72d0bdc6e7b81cf04b4b742010d73467ad09aad
Imran Shaikh <95980002+itsimran02@users.noreply.github.com>
2024-02-29 10:35:54 +0530

    [Feature] : Header component comments resolved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
 import useWindowResize from '../utils/windowResizeUtils';
 import { SCREEN_SIZE, NAV_ITEMS } from '../constants/app-constant';
-import { useLocation } from 'react-router-dom';
+import { ROUTES } from '../constants/app-constant';
 
 import { brandLogo } from '../assets';
 
@@ -10,8 +12,6 @@ const NAV_ITEMS_EXCLUDING_LAST = NAV_ITEMS.slice(0, -1);
 const Header = () => {
     const windowWidth = useWindowResize();
     const [isMenuOpen, setMenuOpen] = useState(false);
-
-    const location = useLocation();
 
     const toggleMenu = () => {
         setMenuOpen(!isMenuOpen);
@@ -24,43 +24,43 @@ const Header = () => {
     }, [windowWidth]);
 
     return (
-        <div className="w-full sticky top-0 z-50">
+        <header className="w-full sticky top-0 z-50">
             <div className="flex justify-between p-6 md:py-5 md:px-16 lg:px-20 xl:px-28 2xl:px-36 items-center bg-white bg-opacity-50 backdrop-blur-md">
-                <a href="/">
+                <Link to={ROUTES.HOME}>
                     <img src={`${brandLogo}`} alt="Brand logo" className="object-cover h-5 lg:h-6" />
-                </a>
+                </Link>
 
                 <div className="flex justify-end items-center text-center md:gap-6 lg:gap-8 xl:gap-12 2xl:gap-16">
                     <button
                         onClick={toggleMenu}
                         className={`flex md:hidden flex-col gap-1 cursor-pointer 
                                 ${isMenuOpen ? 'rotate-45' : ''}`}>
-                        <div
+                        <span
                             className={`w-6 h-[3px] bg-accent-blue transition duration-300 opacity
-                            ${isMenuOpen ? '-rotate-90 translate-y-1.5 ' : 'translate-y-0'}`}></div>
-                        <div
+                            ${isMenuOpen ? '-rotate-90 translate-y-1.5 ' : 'translate-y-0'}`}></span>
+                        <span
                             className={`w-6 h-[3px] bg-accent-blue transition duration-300 
-                            ${isMenuOpen ? 'opacity-0' : 'translate-y-[5px]'}`}></div>
-                        <div
+                            ${isMenuOpen ? 'opacity-0' : 'translate-y-[5px]'}`}></span>
+                        <span
                             className={`w-6 h-[3px] bg-accent-blue transition duration-300 -translate-y-2
-                            ${isMenuOpen ? '' : '-translate-y-0'}`}></div>
+                            ${isMenuOpen ? '' : '-translate-y-0'}`}></span>
                     </button>
                     <nav className="hidden md:flex  2xl:gap-4">
                         {NAV_ITEMS_EXCLUDING_LAST.map((nav, index) => (
-                            <a
-                                href={nav.link}
+                            <NavLink
+                                to={nav.link}
                                 key={`header-${index}`}
-                                className={`md:text-xs lg:text-sm xl:text-base uppercase hover:font-semibold cursor-pointer md:w-16 lg:w-20 xl:w-24 2xl:w-28 text-header-gray ${nav.link === location.pathname ? 'font-semibold' : 'font-normal'}`}>
+                                className="md:text-xs lg:text-sm xl:text-base uppercase hover:font-semibold cursor-pointer md:w-16 lg:w-20 xl:w-24 2xl:w-28 text-header-gray font-normal">
                                 {nav.title}
-                            </a>
+                            </NavLink>
                         ))}
                     </nav>
-                    <a
-                        href="/contact"
+                    <Link
+                        to={ROUTES.CONTACT}
                         className="hidden md:flex md:rounded-3xl xl:rounded-5xl bg-brand-blue transition duration-500 ease-in-out font-base transform focus:scale-100 hover:bg-gradient-to-r hover:from-brand-blue hover:to-brand-pink md:text-xs xl:text-sm text-white px-6 py-3.5
                         ">
                         CONTACT US
-                    </a>
+                    </Link>
                 </div>
             </div>
 
@@ -70,16 +70,17 @@ const Header = () => {
                         px-9 pt-24 text-center text-3xl text-fade-gray tracking-tight leading-10 gap-6 font-light items-start md:hidden h-screen
                         ">
                     {NAV_ITEMS_EXCLUDING_LAST.map((nav, index) => (
-                        <a
-                            href={nav.link}
+                        <NavLink
+                            to={nav.link}
                             key={`menu-${index}`}
-                            className={`uppercase hover:font-medium cursor-pointer ${nav.link === location.pathname ? 'font-semibold' : 'font-extralight'}`}>
+                            className="uppercase hover:font-semibold cursor-pointer font-extralight"
+                            onClick={toggleMenu}>
                             {nav.title}
-                        </a>
+                        </NavLink>
                     ))}
                 </nav>
             )}
-        </div>
+        </header>
     );
 };
 
