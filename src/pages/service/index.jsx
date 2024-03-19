4c3bd811840cd60c44f92551d0bbdb56a3dc99a8
Himanshu <hs3928782@gmail.com>
2024-03-19 13:24:36 +0530

    [Feature] : Add research section component

--- src/pages/service/index.jsx
+++ src/pages/service/index.jsx
@@ -1,9 +1,11 @@
 import HeroSection from './HeroSection';
+import ResearchSection from './ResearchSection';
 
 const Service = () => {
     return (
         <>
             <HeroSection />
+            <ResearchSection/>
         </>
     );
 };
