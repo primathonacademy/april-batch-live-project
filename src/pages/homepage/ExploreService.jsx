df8c3b83078ee4279165c55517263601c970d050
Himanshu <hs3928782@gmail.com>
2024-03-21 11:06:17 +0530

    [Feature] : Improve animation in explore service section

--- src/pages/homepage/ExploreService.jsx
+++ src/pages/homepage/ExploreService.jsx
@@ -1,44 +1,55 @@
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from '@gsap/react';
 import { ourServiceImage1, ourServiceImage2, ourServiceImage3, rightArrowIcon } from '../../assets';
-import { scrollHandler } from '../../utils/ScrollHandler';
-import useWindowResize from '../../utils/windowResizeUtils';
+import { useRef } from 'react';
 
 const ExploreService = () => {
+    const containerRef = useRef(null);
     useGSAP(() => {
-        if (useWindowResize) {
-            gsap.registerPlugin(ScrollTrigger);
-            const cleanupScrollHandler = scrollHandler();
-            return () => {
-                cleanupScrollHandler();
-            };
-        }
+        const container = containerRef.current;
+        const images = container.querySelectorAll('.image-container');
+        gsap.from(images, {
+            y: 200,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.5,
+            scrollTrigger: {
+                trigger: container,
+                start: 'top 90%',
+                end: 'top 20%',
+                scrub: 1,
+                once: true
+            }
+        });
     }, []);
     return (
         <>
             <div className="px-9 md:px-16 lg:px-20 xl:px-28 2xl:px-36 pt-12 pb-8">
-                <div className="flex flex-col md:flex-row items-stretch justify-center mt-16 gap-2 mx-4 overflow-hidden">
-                    <div className="bg-fade-white w-full md:w-7/12 lg:w-[506px] mt-16 py-4 rounded-xl flex flex-col justify-center items-center service-item-1">
-                        <h2 className="lg:mt-6 md:mt-24 text-3xl md:text-5xl px-6 md:px-20 font-poppins">
+                <div className="flex flex-col md:flex-row gap-2 overflow-hidden" ref={containerRef}>
+                    <div className="bg-fade-white lg:w-[38%] 2xl:w-[90%] lg:pt-20 pt-10 2xl:pt-40 rounded-xl flex flex-col image-container">
+                        <h2 className="lg:mt-6 md:mt-24 text-2xl lg:text-4xl xl:text-5xl md:text-4xl lg:px-16 px-10 font-poppins">
                             Innovation is the New Marketing.
                         </h2>
-                        <p className="mt-3 md:mt-9 text-red-gray font-semibold opacity-60 text-lg md:text-2xl px-6 md:px-20 font-roboto">
+                        <p className="mt-4 md:mt-16 text-our-service-paragraph sm:text-lg md:text-lg lg:px-16 px-10 font-roboto">
                             Build your brand moat with us and leave the competition in the dust.
                         </p>
 
-                        <span className="flex lg:mt-6 md:mt-12 items-center justify-center hover:bg-gradient-to-r from-fade-blue-white from-30% to-pink-200 bg-fade-blue-white mx-6 md:mx-40 py-3 w-52 lg:w-60 gap-6 lg:gap-12 rounded-full service-item-2">
-                            <h3 className="text-dark-blue font-semibold font-poppins text-sm ">ALL OUR SERVICES</h3>
+                        <span className="flex lg:mt-32 md:mt-16 mt-12 mb-6 items-center justify-center hover:bg-gradient-to-r from-fade-blue-white from-30% to-pink-200 bg-fade-blue-white mx-auto py-2 w-44 lg:w-60 gap-0 lg:gap-8 rounded-full">
+                            <h3 className="text-dark-blue font-semibold font-poppins text-sm">ALL OUR SERVICES</h3>
                             <a href={'/service'}>
                                 <div className="bg-brand-blue rounded-full ml-2 p-3 hover:bg-gradient-to-r from-pink-300 from-30% to-brand-blue">
-                                    <img src={rightArrowIcon} alt="arrow icon" className="w-4 h-4 hover:scale-125" />
+                                    <img
+                                        src={rightArrowIcon}
+                                        alt="arrow icon"
+                                        className="w-2 h-2 lg:h-4 lg:w-4 hover:scale-125"
+                                    />
                                 </div>
                             </a>
                         </span>
                     </div>
 
-                    <div className="mt-8 md:mt-16 w-full md:w-[400px] flex md:flex-col sm:flex-row items-center gap-3 relative service-item-1">
-                        <div className="w-full lg:h-80 md:h-72 h-64 relative  bg-white rounded-xl">
+                    <div className=" w-full flex md:flex-col sm:flex-row items-center gap-3 relative image-container">
+                        <div className="w-full lg:h-80 md:h-72 h-64 relative bg-white rounded-xl">
                             <img
                                 src={ourServiceImage1}
                                 alt="service image"
@@ -53,7 +64,7 @@ const ExploreService = () => {
                                 </span>
                             </div>
                         </div>
-                        <div className="w-full lg:h-80 md:h-72 h-64 relative bg-white rounded-xl">
+                        <div className="w-full  lg:h-80 md:h-72 h-64 relative bg-white rounded-xl">
                             <img
                                 src={ourServiceImage2}
                                 alt="service image"
@@ -69,9 +80,13 @@ const ExploreService = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="bg-fade-orange w-full md:w-[400px] mt-8 md:mt-16 rounded-xl flex justify-center items-center overflow-hidden relative  service-item-1">
-                        <div className="overflow-hidden" style={{ marginRight: '-200px' }}>
-                            <img src={ourServiceImage3} alt="service image" className="w-full h-full" />
+                    <div className="bg-fade-orange w-full lg:h-auto md:h-auto h-48 rounded-xl flex justify-center items-center overflow-hidden relative image-container">
+                        <div className="overflow-hidden" style={{ marginRight: '-100px' }}>
+                            <img
+                                src={ourServiceImage3}
+                                alt="service image"
+                                className="lg:w-full lg:h-full h-64 w-64 py-6 object-cover"
+                            />
                             <div className="absolute bottom-0 left-0 p-4 text-white">
                                 <span className="text-sm md:text-lg lg:text-xl font-semibold">UX/UI</span>
                             </div>
