5ed40aec0c003ea624d516005608dd660810164a
dhananhub <dkjha444@gmail.com>
2024-03-22 15:38:33 +0530

    [feature] : Add stratagy in case study page

--- src/pages/case-study/index.jsx
+++ src/pages/case-study/index.jsx
@@ -1,5 +1,9 @@
+import Strategy from '../../pages/case-study/Strategy/index';
+
+
 const CaseStudy = () => {
-    return <div className="px-6 md:px-16 lg:px-20 xl:px-28 2xl:px-36">Welcome to Case Study</div>;
+    return <div className="px-6 md:px-16 lg:px-20 xl:px-28 2xl:px-36"> <Strategy/>
+     </div>;
 };
 
 export default CaseStudy;
